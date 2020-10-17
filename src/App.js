56de19236307f9202bf61a13ad2d8735eebfe5b6
Gabriel Perinazzo <perinazzo.gbrl@gmail.com>
2020-10-17 19:56:37 -0300

    desafio concluido (ta passando nos testes)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,87 @@
 import React from "react";
+import axios from 'axios';
+
+import api from './services/api';
+
+import Icon from './components/Icon';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = React.useState([]);
+  const titleRef = React.useRef(null);
+  const techsRef = React.useRef(null);
+
+  React.useEffect(() => {
+    (async () => {
+      const response = await api.get('/repositories');
+
+      const data = response?.data?.map((item) => ({
+        ...item,
+        techsString: item.techs?.join(', '),
+      }))
+
+      setRepositories(data);
+    })();
+  }, []);
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const response = await api.post('/repositories', {
+      title: titleRef?.current?.value,
+      techs: techsRef?.current?.value?.split(','),
+      url: 'https://github.com/perinazzoo'
+    });
+
+    const repository = {
+      ...response?.data,
+      techsString: response?.data?.techs?.join(', '),
+    }
+
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`/repositories/${id}`);
+
+      const newData = repositories.filter((r) => r?.id !== id);
+
+      setRepositories(newData);
+    } catch (err) {
+      alert('Ocorreu um erro ao deletar.');
+    }
   }
 
   return (
-    <div>
+    <div className="container">
+      <h1>Lista de repositórios</h1>
+
+      <div>
+        <div>
+          <input ref={titleRef} placeholder="Insira o título do projeto" type="text" className="input-title" />
+          <input ref={techsRef} placeholder="Tecnologias utilizadas no projeto, separadas por vírgula" type="text" className="input-techs" />
+        </div>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories?.map((item) => (
+          <li key={item?.id}>
+            <div>
+              <a href={item?.url || '#'}><h2 className="item-title">{item?.title}</h2></a>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+              <h3 className="item-techs">Tecnologias: </h3> <p className="item-tech-list">{item?.techsString}</p>
+            </div>
+
+            <button onClick={() => handleRemoveRepository(item?.id)}>
+              <Icon className="icon" />
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
